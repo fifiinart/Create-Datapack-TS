2b38d73d10811424864f44bf58e4fb5668477a9a
Fifi Art <fifiinart@gmail.com>
2021-01-20 19:59:28 -0500

    Add shapeless and shaped crafting recipes

---
+++ src/recipes/CraftingShaped.ts
@@ -0,0 +1,39 @@
+import { Recipe, RecipeObject } from "../Recipe";
+import { CountableItem, Item } from "./Item";
+export interface CraftingShapedObject extends RecipeObject {
+  pattern: string[];
+  key: Array<Item | Item[]>
+  result: CountableItem
+}
+export class CraftingShapedRecipe implements CraftingShapedObject, Recipe<CraftingShapedObject> {
+  type = "minecraft:crafting_shapeless"
+  group: string
+  pattern: string[]
+  key: Array<Item | Item[]>
+  result: CountableItem
+
+  constructor(recipeObject: Omit<CraftingShapedObject, "type">)
+  constructor(group: string, pattern: string[], key: Array<Item | Item[]>, result: CountableItem)
+  constructor(recipeObjectOrGroup: Omit<CraftingShapedObject, "type"> | string, pattern?: string[], key?: Array<Item | Item[]>, result?: CountableItem) {
+    if (typeof recipeObjectOrGroup === "object") {
+      this.pattern = recipeObjectOrGroup.pattern;
+      this.group = recipeObjectOrGroup.group;
+      this.key = recipeObjectOrGroup.key;
+      this.result = recipeObjectOrGroup.result;
+    } else {
+      if (pattern == undefined || key == undefined || result == undefined) throw new Error("Not all parameters were passed in at CraftingShapelessRecipe()")
+      this.pattern = pattern;
+      this.group = recipeObjectOrGroup;
+      this.key = key;
+      this.result = result;
+    }
+  }
+
+  toJSON = () => ({
+    type: this.type,
+    pattern: this.pattern,
+    group: this.group,
+    key: this.key,
+    result: this.result
+  })
+}
---
+++ src/recipes/CraftingShapeless.ts
@@ -0,0 +1,35 @@
+import { Recipe, RecipeObject } from "../Recipe";
+import { CountableItem, Item } from "./Item";
+export interface CraftingShapelessObject extends RecipeObject {
+  ingredients: Array<Item | Item[]>
+  result: CountableItem
+}
+export class CraftingShapelessRecipe implements CraftingShapelessObject, Recipe<CraftingShapelessObject> {
+  type = "minecraft:crafting_shapeless"
+  group: string
+  ingredients: Array<Item | Item[]>
+  result: CountableItem
+
+  constructor(recipeObject: Omit<CraftingShapelessObject, "type">)
+  constructor(group: string, ingredients: Array<Item | Item[]>, result: CountableItem)
+  constructor(recipeObjectOrGroup: Omit<CraftingShapelessObject, "type"> | string, ingredients?: Array<Item | Item[]>, result?: CountableItem) {
+    if (typeof recipeObjectOrGroup === "object") {
+      this.group = recipeObjectOrGroup.group;
+      this.ingredients = recipeObjectOrGroup.ingredients;
+      this.result = recipeObjectOrGroup.result;
+    } else {
+      if (ingredients == undefined || result == undefined) throw new Error("Not all parameters were passed in at CraftingShapelessRecipe()")
+      if (ingredients.length < 1 || ingredients.length > 9) throw new Error("Invalid number of ingredients at CraftingShapelessRecipe()")
+      this.group = recipeObjectOrGroup;
+      this.ingredients = ingredients;
+      this.result = result;
+    }
+  }
+
+  toJSON = () => ({
+    type: this.type,
+    group: this.group,
+    ingredients: this.ingredients,
+    result: this.result
+  })
+}
